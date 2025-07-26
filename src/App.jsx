f7ae38d489128f3be3e4520eab2b4dcd71330450
Ahmed-HA-RE <ah607k@gmail.com>
2025-07-26 22:39:16 +0400

    feat:add a wrapping container in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import ProductList from './components/ProductList';
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-gray-100 p-6'>
-      <h1 className='text-3xl font-bold mb-6'>🛒 Product Catolog</h1>
-
-      <ProductList />
+    <div className='min-h-screen bg-gray-100'>
+      <div className=' container mx-auto  p-6'>
+        <h1 className='text-3xl font-bold mb-6'>🛒 Product Catalog</h1>
+        <ProductList />
+      </div>
     </div>
   );
 };
