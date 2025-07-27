9f7e5adc283f087155f5d72cdb0f093d71a4ab75
Ahmed-HA-RE <ah607k@gmail.com>
2025-07-27 17:55:39 +0400

    feat(localStorage):implement localStorage to the cart

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,9 +1,16 @@
-import { useContext, useState, createContext } from 'react';
+import { useContext, useState, createContext, useEffect } from 'react';
 
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    return cart || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
 
   const addToCart = (product) => {
     setCart((prevCart) => {
