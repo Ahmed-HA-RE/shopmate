90fdc9e5d6195c1ad27c8bb71c1bb5553b01f896
Ahmed-HA-RE <ah607k@gmail.com>
2025-07-27 12:30:45 +0400

    feat:add and create a cart logic to check if the item exists in the cart or not

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -5,7 +5,25 @@ const CartContext = createContext();
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
-  return <CartContext.Provider value={cart}>{children}</CartContext.Provider>;
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existing = prevCart.find((item) => item.id === product.id);
+
+      if (existing) {
+        return prevCart.map((item) => {
+          return item.id === product.id ? { ...item, qty: item.qty + 1 } : item;
+        });
+      } else {
+        return [...prevCart, { ...product, qty: 1 }];
+      }
+    });
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 }
 
 export const useCart = () => useContext(CartContext);
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,8 @@
+import { useCart } from '../Context/CartContext';
+
 const ProductCard = ({ product }) => {
+  const { addToCart } = useCart();
+
   return (
     <div className='bg-white rounded-lg shadow p-4 flex flex-col'>
       <img
@@ -9,7 +13,10 @@ const ProductCard = ({ product }) => {
       <h2 className='text-xl font-semibold'>{product.name}</h2>
       <p className='text-gray-500 text-sm mb-3'>{product.description}</p>
       <h3 className='text-xl font-bold mb-4'>${product.price}</h3>
-      <button className='capitalize cursor-pointer w-full bg-blue-600 text-white p-3 rounded'>
+      <button
+        onClick={() => addToCart(product)}
+        className='capitalize cursor-pointer w-full bg-blue-600 text-white p-3 rounded hover:bg-blue-700 active:scale-95 transition duration-200 '
+      >
         add to cart
       </button>
     </div>
